2aa9169c4b15d084544c865568d6e2b7d3c42e4a
Abdelaziz Abdelkarim <abdelazizabdelkarim2001@gmail.com>
2025-09-08 23:59:04 +0300

    css change in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
+
 import { ArrowUp } from "lucide-react";
+
 export const Footer = () => {
-    return (
-        <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
-            <p className="text-sm text-muted-foreground all">© 2025 Abdelaziz Abdelkarim. All rights reserved.</p>
-            <a href="#hero" className="text-primary p-2 rounded-full bg-primary/10 hover:bg-primary/20 transition-colors">
-                <ArrowUp />
-            </a>
-        </footer>
-    );
-}
+  return (
+    <footer className="py-12 px-4  relative  border-border mt-12 pt-8 flex flex-wrap justify-between items-center ">
+      <p className="text-sm text-muted-foreground">
+        &copy; {new Date().getFullYear()} Abdelaziz Abdelkarim.. All rights reserved.
+      </p>
+      <a
+        href="#hero"
+        className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors"
+      >
+        <ArrowUp size={20} />
+      </a>
+    </footer>
+  );
+};
